193133d627c2e626996b77f75ee2a624ff575768
Pedro jr <pedro81565@hotmail.com.com>
2021-06-11 17:17:28 -0300

    update  delete

--- src/main/java/br/com/springboot/api/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/api/controllers/GreetingsController.java
@@ -5,12 +5,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -60,4 +63,22 @@ public class GreetingsController {
     	return new ResponseEntity<Users>(user, HttpStatus.CREATED);
     	
     }
+    
+    @PutMapping(value = "update")
+    @ResponseBody
+    public ResponseEntity<Users> update(@RequestBody Users users) {
+    	Users user = usersRepository.saveAndFlush(users);
+    	return new ResponseEntity<Users>(user, HttpStatus.OK);
+    	
+    }
+    
+    @DeleteMapping(value = "delete")
+    @ResponseBody
+    public ResponseEntity<String> delete(@RequestParam Long iduser) { 	
+    	usersRepository.deleteById(iduser);
+    	return new ResponseEntity<String>("User deletado com sucesso", HttpStatus.OK);
+    	
+    }
+    
+    
 }
